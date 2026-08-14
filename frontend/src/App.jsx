15c710a910e66c6f0a2a967ec988f7b62a785495
sirichandana-ops <sirichandanamothe@gmail.com>
2026-08-14 14:23:52 +0530

    Connect frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     setAnswer("");
 
     try {
-      const response = await fetch("https://askmynotes2-yayj.onrender.com", {
+      const response = await fetch("https://askmynotes2-yayj.onrender.com/ask", {
         method: "POST",
 
         headers: {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  serverl: {
+  server: {
     allowHosts: ['https://askmynotes2-1-uqfa.onrender.com'],
   },
 })
