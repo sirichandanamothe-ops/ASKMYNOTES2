c4d00e5c9f0f701483b27e36ba1c5fea710c0513
sirichandana-ops <sirichandanamothe@gmail.com>
2026-08-14 11:23:42 +0530

    Modified Application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  serverl: {
+    allowHosts: ['https://askmynotes2-1-uqfa.onrender.com'],
+  },
 })
